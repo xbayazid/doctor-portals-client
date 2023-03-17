7b6ea1675e5f015e39480c50a7e21588b2cd0f8b
xbayazid <bayazid9006@gmail.com>
2023-03-17 20:18:35 +0600

    manage doctor designed

---
+++ src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
@@ -0,0 +1,69 @@
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import Loading from '../../Shared/Loading/Loading';
+
+const ManageDoctors = () => {
+    const { data: doctors, isLoading } = useQuery({
+        queryKey: ['doctors'],
+        queryFn: async () => {
+            try {
+                const res = await fetch('http://localhost:5000/doctors', {
+                    headers: {
+                        authorization: `bearer ${localStorage.getItem('accessToken')}`
+                    }
+                });
+                const data = await res.json();
+                return data;
+            }
+            catch (error) {
+
+            }
+        }
+    });
+
+    if(isLoading){
+        return <Loading/>
+    }
+    
+    return (
+        <div>
+            <h2 className="text-3xl">Manage Doctors: {doctors?.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full mt-5">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Speciality</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            doctors.map((doctor, i) => <tr key={doctor._id}>
+                                <th>{i + 1}</th>
+                                <td><div className="avatar">
+                                    <div className="w-24 rounded-full">
+                                        <img src={doctor.image} alt="" />
+                                    </div>
+                                </div></td>
+                                <td>{doctor.name}</td>
+                                <td>{doctor.email}</td>
+                                <td>{doctor.specialty}</td>
+                                <td>
+                                    <label htmlFor="confirmation-modal" className="btn btn-sm btn-error">Delete</label>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default ManageDoctors;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -5,6 +5,7 @@ import Appointment from "../../Pages/Appointment/Appointment/Appointment";
 import AddDoctor from "../../Pages/Dashboard/AddDoctor/AddDoctor";
 import AllUsers from "../../Pages/Dashboard/AllUsers/AllUsers";
 import Dashboard from "../../Pages/Dashboard/Dashboard/Dashboard";
+import ManageDoctors from "../../Pages/Dashboard/ManageDoctors/ManageDoctors";
 import MyAppointment from "../../Pages/Dashboard/MyAppointment/MyAppointment";
 import Error from "../../Pages/Error/Error";
 import Home from "../../Pages/Home/Home/Home";
@@ -54,7 +55,12 @@ const router = createBrowserRouter([
                 path: '/dashboard/adddoctor',
                 element: <AdminRoute><AddDoctor></AddDoctor></AdminRoute>
 
-            }
+            },
+            {
+                path: '/dashboard/managedoctors',
+                element: <AdminRoute><ManageDoctors></ManageDoctors></AdminRoute>
+
+            },
         ]
     },
     {
